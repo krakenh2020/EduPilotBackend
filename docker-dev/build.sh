39f6917177211d9adf0b5844684d5a5b0125b89e
Stefan More <stefan.more@iaik.tugraz.at>
2021-08-24 16:27:19 +0200

    fixed typo

--- docker-dev/build.sh
+++ docker-dev/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# We need to creat the user inside the container with the same ID as the host user
+# We need to create the user inside the container with the same ID as the host user
 docker-compose build --build-arg "UID=${SUDO_UID:-$UID}"
 
 # Install dependencies
