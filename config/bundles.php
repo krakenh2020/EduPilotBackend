77a16c4adcd1081f824ee47baddef609b0e368a4
Stefan More <stefan.more@iaik.tugraz.at>
2021-08-24 14:45:51 +0200

    Switch to the new dbp package names, updated some stuff

--- config/bundles.php
+++ config/bundles.php
@@ -3,13 +3,14 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => false],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Dbp\Relay\KeycloakBundle\DbpRelayKeycloakBundle::class => ['all' => true],
+    Dbp\Relay\CoreBundle\DbpRelayCoreBundle::class => ['all' => true],
     VC4SM\Bundle\Vc4smBundle::class => ['all' => true],
-    DBP\API\CoreBundle\DbpCoreBundle::class => ['all' => true],
 ];
