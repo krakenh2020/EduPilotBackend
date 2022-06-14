cc0027e34398f9956e8abf21eedec62b9bae6f46
Stefan More <stefan.more@iaik.tugraz.at>
2022-06-14 22:02:13 +0200

    symfony flex fix
    https://symfony.com/blog/the-old-flex-infrastructure-is-shutting-down

--- docker-dev/build.sh
+++ docker-dev/build.sh
@@ -3,5 +3,7 @@
 # We need to create the user inside the container with the same ID as the host user
 docker-compose build --build-arg "UID=${SUDO_UID:-$UID}"
 
+docker-compose run --rm --entrypoint composer php-fpm update symfony/flex --no-plugins --no-scripts
+
 # Install dependencies
 docker-compose run --rm --entrypoint composer php-fpm install
